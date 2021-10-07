9faac842aec3aba1bc2735a4fa4bdb660bf5c900
Elif <elifcevik48@gmail.com>
2021-10-07 20:11:48 +0300

    Course 1

---
+++ src/main/java/io/pera/c01/Task.java
@@ -0,0 +1,29 @@
+package io.pera.c01;
+
+public class Task {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 7; i++) {
+            for (int star = 0; star < (i); star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+}
+
+// OUTPUT
+/*
+
+ *
+ **
+ ***
+ ****
+ *****
+ ******
+
+ */
