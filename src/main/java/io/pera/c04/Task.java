60a56223813cc50a42e4dfe96fb5e70df1d6c40b
Elif <elifcevik48@gmail.com>
2021-10-07 20:12:14 +0300

    Course 4

---
+++ src/main/java/io/pera/c04/Task.java
@@ -0,0 +1,34 @@
+package io.pera.c04;
+
+public class Task {
+
+    // prime numbers from 1 to 100
+    public static void main(String[] args) {
+        for(int number=2; number<=100; number++)
+        {
+            int flag = 0;
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+
+            if(flag==0)
+            {
+                System.out.print(number+", ");
+            }
+        }
+        System.out.println();
+    }
+
+}
+
+// OUTPUT
+/*
+
+2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+
+ */
